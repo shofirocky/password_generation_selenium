44e46aa775a9e7a1e96175509113b5f3b479f26a
Shofi Ahmed Rocky <shofirocky@gmail.com>
2025-10-02 12:53:02 +0600

    Update genpass_checker.py

--- genpass_checker.py
+++ genpass_checker.py
@@ -81,3 +81,47 @@ generate_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Gene
 generate_button.click()
 time.sleep(2)
 """
+
+"""
+toggle1 = driver.find_element(By.XPATH, "(//button[@role='switch' and @aria-checked])[1]")
+toggle2 = driver.find_element(By.XPATH, "(//button[@role='switch' and @aria-checked])[2]")
+toggle3 = driver.find_element(By.XPATH, "(//button[@role='switch' and @aria-checked])[3]")
+
+state1 = toggle1.get_attribute("aria-checked")  # "true" or "false"
+state2 = toggle2.get_attribute("aria-checked")
+state3 = toggle3.get_attribute("aria-checked")
+
+if state1 == "true":   # If on
+    toggle1.click()
+if state2 == "true":
+    toggle2.click()
+if state3 == "false":
+    toggle3.click()
+
+generate_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Generate')]")
+generate_button.click()
+time.sleep(2)
+
+password = driver.find_element(By.XPATH, "//code[@class='font-mono text-xl text-white flex-1 overflow-x-auto']")
+has_upper = any(ch.isupper() for ch in password.text)
+has_lower = any(ch.islower() for ch in password.text)
+has_digit = any(ch.isdigit() for ch in password.text)
+if has_upper:
+    print("matched:", password.text)
+
+elif has_upper and has_digit:
+    print ("2nd matched:", password.text)
+
+elif has_lower and has_digit:
+    print("3rd matched:", password.text)
+
+elif has_digit:
+    print("Only digit:", password.text)
+
+else:
+
+    print("not matched")
+
+time.sleep(2)
+"""
+
